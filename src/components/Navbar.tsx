0a544d9cf4a4da631171a9f054aa20e89f5d27a9
RaumTora <tora@raum.network>
2024-12-27 16:47:08 +0530

    fixed logo to url instead of static file selection

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,7 +18,7 @@ export function Navbar() {
       <div className="max-w-7xl mx-auto px-6 lg:px-8">
         <div className="flex items-center justify-between h-20">
           <div className="flex items-center">
-            <img src='src/public/Group 61.svg' width={240}></img>
+            <img src='https://raw.githubusercontent.com/Raum-Network/RaumNetwork-FrontEnd/refs/heads/main/src/public/Group%2061.svg' width={240}></img>
           </div>
           
           <div className="hidden md:block">
